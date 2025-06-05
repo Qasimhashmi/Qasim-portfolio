d4d09da80bef03271ee9b4d59ded3d476521f596
Pinkfly <pinkfly@Pinkflys-MacBook-Air.local>
2025-06-05 13:51:30 +0500

    working-on-hero-section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+
+const HeroSection = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <h1 className="Navbar-heading">Qasim Ali</h1>
+        <ul className="navigation-list">
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default HeroSection
